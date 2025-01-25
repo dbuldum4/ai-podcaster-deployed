40cdf9735379cd499f6101d8df809edb5a151416
wastella <william.a.stella@gmail.com>
2025-01-25 00:44:50 -0500

    fixed the env variable api key

--- generator.py
+++ generator.py
@@ -1,7 +1,15 @@
-#AIzaSyD-crAs6xwCimaWQZZiQR5LAKGgHZHOERM
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+key = os.getenv("GEMINI_KEY")
+
+print(key)
+
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyD-crAs6xwCimaWQZZiQR5LAKGgHZHOERM")
+genai.configure(api_key=key)
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Explain how AI works")
 print(response.text)

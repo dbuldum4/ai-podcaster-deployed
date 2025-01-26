27109ae81b5639a64519501e6eba1cf4ee403991
Deniz Aydin Buldum <deniz.a.buldum@gmail.com>
2025-01-26 13:35:06 -0500

    env

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 // config.js
-// config.js
 const CONFIG = {
-      GEMINI_KEY: "AIzaSyBoZnjm8b_s0LjDV3fXHcSHDvaA7nTvqTo",
-      CLIENT_ID: "287269128315-arp0g7r5446jkc59ncuckvatio7pm3lo.apps.googleusercontent.com",
-      CLIENT_SECRET: "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",
-      REFRESH_TOKEN: "1//04U6R8A_jnIdkCgYIARAAGAQSNwF-L9Ir23f44qF35ISSVtB3fWpwuWqjkj0Xqu0tCbGPHiUOjPE2KhaYQ5qO0f8zZRWEITYYUzI", // To be obtained in Step 2
-    };
-    
-    export default CONFIG;
+  GEMINI_KEY: import.meta.env.VITE_GEMINI_KEY,
+  CLIENT_ID: import.meta.env.VITE_CLIENT_ID,
+  CLIENT_SECRET: import.meta.env.VITE_CLIENT_SECRET,
+  REFRESH_TOKEN: import.meta.env.VITE_REFRESH_TOKEN,
+};
+
+export default CONFIG;

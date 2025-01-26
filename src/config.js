09f682c9d417064e100838d438b40b7a678f5407
Deniz Aydin Buldum <deniz.a.buldum@gmail.com>
2025-01-26 10:05:34 -0500

    unnested

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// config.js
+// config.js
+const CONFIG = {
+      GEMINI_KEY: "AIzaSyBoZnjm8b_s0LjDV3fXHcSHDvaA7nTvqTo",
+      CLIENT_ID: "287269128315-arp0g7r5446jkc59ncuckvatio7pm3lo.apps.googleusercontent.com",
+      CLIENT_SECRET: "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",
+      REFRESH_TOKEN: "1//04U6R8A_jnIdkCgYIARAAGAQSNwF-L9Ir23f44qF35ISSVtB3fWpwuWqjkj0Xqu0tCbGPHiUOjPE2KhaYQ5qO0f8zZRWEITYYUzI", // To be obtained in Step 2
+    };
+    
+    export default CONFIG;
